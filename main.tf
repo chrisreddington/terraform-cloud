83eae535e40bfcaa7441b69355302e9146242a2b
Chris Reddington <christian.reddington@outlook.com>
2020-03-13 16:18:31 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
-variable "hostname" {
-  type = string
-}
-
-
-# Configure the Terraform Enterprise Provider
-provider "tfe" {
-  hostname = var.hostname  
-  }
-
 # Create an organization
-resource "tfe_organization" "org" {
-  name = "Theatreers"
-  email = "chris@theatreers.com"
-}
+#resource "tfe_organization" "org" {
+#  name = "Theatreers"
+#  email = "chris@theatreers.com"
+#}
 
 # Dummy Test
 resource "tfe_workspace" "test" {
